a7cb4923c1e8a4791a9f02bd26d6894d77d8f14d
babyboo07 <emma28072002@gmail.com>
2022-07-15 13:31:01 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "antd/dist/antd.css";
+import React from "react";
+import { BrowserRouter, Router, Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import Siderbar from "./Component/Layout/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes></Routes>
+
+        <Siderbar />
+      </BrowserRouter>
     </div>
   );
+
+  // return <Siderbar />;
 }
 
 export default App;
---
+++ src/Component/Layout/Sidebar.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Menu, Layout } from "antd";
+import { AreaChartOutlined, UnorderedListOutlined, ContainerOutlined } from "@ant-design/icons";
+import { faCircleUser, faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import logo from "../../logo.svg";
+import { NavLink, useLocation } from "react-router-dom";
+
+const { Sider } = Layout;
+
+const Siderbar = (props) => {
+  return (
+    <Sider
+      style={{
+        width: 256,
+      }}
+      collapsed={props.isCollapse}
+      theme="light"
+    >
+      <div className="logo text-center" style={{ height: props.isCollapse ? 50 : 100 }}>
+        <img src={logo} height={props.isCollapse ? 50 : 100} />
+      </div>
+      <Menu
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["/dashboard"]}
+        mode="inline"
+        theme="light"
+        id="sidebar"
+      >
+        <Menu.Item key="dashboard">
+          <NavLink to="/">
+            <AreaChartOutlined />
+            <span>Dashboard</span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="category">
+          <NavLink to="/category/list">
+            <UnorderedListOutlined />
+            <span>Nhóm máu</span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="product">
+          <NavLink to="/product/list">
+            <ContainerOutlined />
+            <span>Bài viết</span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="order">
+          <NavLink to="/order/list">
+            <span className="anticon">
+              <FontAwesomeIcon icon={faCartArrowDown} />
+            </span>
+            <span>Thông tin liên hệ </span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="account">
+          <NavLink to="/account/list">
+            <span className="anticon">
+              <FontAwesomeIcon icon={faCircleUser} />
+            </span>
+            <span>Quản lý người dùng</span>
+          </NavLink>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+  );
+};
+export default Siderbar;
